e52a020f9b364f6aff7bd4270f19c3578c71748e
Maxim Dulchevskiy <mdulchevskiy@gmail.com>
2024-07-26 11:13:00 +0300

    SPC-00001. Project base initialization p.3

--- sandbox/settings/django.py
+++ sandbox/settings/django.py
@@ -9,7 +9,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -19,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sandbox',
 ]
 
 MIDDLEWARE = [
@@ -50,18 +50,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'sandbox.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
